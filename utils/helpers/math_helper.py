09515242872f11035f000086692879c3f19ec031
darlin4 <darlindavid45@gmail.com>
2025-10-13 18:14:15 -0500

    Agrego el grafo a models

---
+++ utils/helpers/math_helper.py
@@ -0,0 +1,9 @@
+"""
+Helper para operaciones matemáticas.
+"""
+
+# Aquí iría la implementación del MathHelper
+# - Cálculos de distancia euclidiana
+# - Conversiones de coordenadas
+# - Cálculos de energía y recursos
+# - Funciones trigonométricas para el mapa
