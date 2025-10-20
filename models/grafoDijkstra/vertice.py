5461186f9a136e58774a50c5dfddc3342ce0521f
darlin4 <darlindavid45@gmail.com>
2025-10-20 14:11:05 -0500

    modifique la clase vertice ajustando al algoritmo de Dijkstra

--- models/grafoDijkstra/vertice.py
+++ models/grafoDijkstra/vertice.py
@@ -1,15 +1,27 @@
-class Vertex:
+class Vertice:
     def __init__(self, id):
-        self.id = id
-        self.adjacent = {}  # dict: neighbor_id -> weight
+        self.id = id                      # nombre estrella
+        self.adjacent = {}                # diccionario: {vertice_vecino: peso}
+        self.distancia = float('inf')     # distancia
+        self.visitado = False             # marca si ya fue visitado
+        self.anterior = None              # vértice anterior en el camino más corto
 
-    def add_neighbor(self, neighbor, weight=0):
-        """Agregar un vecino (por id) con su peso."""
-        self.adjacent[neighbor] = weight
+    def add_neighbor(self, vecino, peso=0):
+        """Agrega un vecino con su peso (distancia)."""
+        self.adjacent[vecino] = peso
 
     def get_connections(self):
-        """Devuelve el diccionario de adyacencias (neighbor_id -> weight)."""
-        return self.adjacent
+        """Devuelve los vértices vecinos."""
+        return self.adjacent.keys()
 
     def get_id(self):
+        """Devuelve el nombre de la estrella."""
         return self.id
+
+    def get_peso(self, vecino):
+        """Devuelve el peso de la conexión a un vecino."""
+        return self.adjacent[vecino]
+
+    def __str__(self):
+        """Representación en texto del vértice y sus conexiones."""
+        return f"{self.id} conectado a {[x.id for x in self.adjacent]}"
